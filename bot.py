ca273d9c768fa7daa47a6ba8cc0f6c93aa03befa
Zachary <rick.jsventures.com>
2021-10-14 23:05:21 -0400

    added hello! comamnd

--- bot.py
+++ bot.py
@@ -13,18 +13,18 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content[0] == '!':
-        if message.channel != CHANNEL:
-            response = f'Hello {name}, please only contact me in bot commands from now on. Thank you'
-            print(response)
 
-            await message.channel.send(response)
 
         content = message.content[1:].lower()
         print(content)
 
         name = message.author.name
+        if message.content[0] == '!':
+            if message.channel != CHANNEL:
+                response = f'Hello {name}, please only contact me in bot commands from now on. Thank you'
+                print(response)
 
+            await message.channel.send(response)
         if content == 'hey' or content == 'hello' or content == 'hi':
             response = f'Hello {name}'
             print(response)
